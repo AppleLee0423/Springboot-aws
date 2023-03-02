4d8ad50ba9838807d8e702e2f3c4beee27672497
AppleLee0423 <ld210@naver.com>
2023-03-02 23:03:28 +0900

    JPA unit test put(update), get(select)

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    //Entity의 일부만 받아 처리하므로 생성자로 Entity를 받아 처리
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
