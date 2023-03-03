641abf41df93312c84047734ee978ec57883a72d
AppleLee0423 <ld210@naver.com>
2023-03-03 21:12:57 +0900

    Index mustache unit test

---
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩(){
+        //when
+        String body = this.restTemplate.getForObject("/",String.class);
+
+        //then
+        //해당 문자열이 존재하는지 확인
+        assertThat(body).contains("Spring Boot Web Service");
+    }
+}
