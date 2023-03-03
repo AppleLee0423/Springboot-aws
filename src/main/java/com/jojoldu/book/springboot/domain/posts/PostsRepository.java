6f97734ba579ad33b2f91379309727ef7ca3b54a
AppleLee0423 <ld210@naver.com>
2023-03-03 23:11:59 +0900

    FindAllData Index Mustache set

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,16 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
     // DAO = Repository(Interface)
     // Entity 클래스와 함께 위치해야 함
+
+    // @Query를 사용하면 가독성이 좋다
+    // 규모가 있는 데이터 조희는 querydsl 프레임워크를 사용할 수 있다. + Mybatis
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,31 @@
+//다른 js와 init 충돌을 방지하기 위해 변수속성으로 function 추가(유효범위 생성)
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
